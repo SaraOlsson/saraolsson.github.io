bad486e458c2da0dbe0c50c35358c4ee7e1b3af7
SaraOlsson <sarol688@student.liu.se>
2019-12-15 22:44:39 +0100

    add react build folder

---
+++ precache-manifest.b071508ea537269af0a7f3d0288cb875.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83e044e7460b8066bc0e7efa583ebd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04c20f36c8830cf9e216",
+    "url": "/static/css/main.e77ddbd3.chunk.css"
+  },
+  {
+    "revision": "bf6f6d3e0c24ad4bc8b4",
+    "url": "/static/js/2.a43d60fe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a43d60fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "04c20f36c8830cf9e216",
+    "url": "/static/js/main.17b6e1ac.chunk.js"
+  },
+  {
+    "revision": "c9913191e565de632244",
+    "url": "/static/js/runtime-main.170f8412.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
